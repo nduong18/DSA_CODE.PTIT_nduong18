ab99f0f9e515aea7327e7932348e9987f6556969
nduong18 <duonghocbai18@gmail.com>
2025-02-04 10:43:43 +0700

    init project

---
+++ baitap.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n,x; cin >> n >> x;
+        vector <pair<int,int>> v;
+        for (int i = 0; i < n; i++){
+            int y; cin >> y;
+            v.push_back({y,abs(x-y)});
+        }
+        stable_sort(v.begin(),v.end(),[](auto a, auto b){return a.second < b.second;});
+        for (auto it : v) cout << it.first << ' ';
+        cout << "\n";
+    }
+}
