844c3f3086bb5636715742284f2ba920ad4d7a9c
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-06 11:29:15 +0700

    Update DSA06047 - BỘ BA SỐ PYTAGO.cpp

--- DSA06047 - BỘ BA SỐ PYTAGO.cpp
+++ DSA06047 - BỘ BA SỐ PYTAGO.cpp
@@ -2,6 +2,18 @@
 #define ll long long
 using namespace std;
 
+bool check(ll a[], ll n){
+    for (ll i = 0; i < n-2; i++){
+            for (ll j = i+1; j < n-1; j++){
+                ll c = a[i] + a[j];
+                if (binary_search(a,a+n,c)){
+                    return true;
+                }
+            }
+        }
+        return false;
+}
+
 int main(){
     int t; cin >> t;
     while (t--){
@@ -12,18 +24,7 @@ int main(){
             a[i] = a[i] * a[i];
         }
         sort(a,a+n);
-        bool check = false;
-        for (ll i = 0; i < n-2; i++){
-            for (ll j = i+1; j < n-1; j++){
-                ll c = a[i] + a[j];
-                if (binary_search(a,a+n,c)){
-                    check = true;
-                    break;
-                }
-            }
-            if (check) break;
-        }
-        if (check) cout << "YES\n";
+        if (check(a,n)) cout << "YES\n";
         else cout << "NO\n";
     }
 }
