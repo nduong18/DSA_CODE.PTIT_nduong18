a6d6bc03831dbc240ebfef33440d6eabc7a84185
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-06 10:13:24 +0700

    Create DSA01008 - XÂU NHỊ PHÂN CÓ K BIT 1.cpp

---
+++ DSA01008 - XÂU NHỊ PHÂN CÓ K BIT 1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int n, final = 0, a[20];
+
+void ktao(){
+    for (int i = 1; i <= n; i++) a[i] = 0;
+}
+
+void sinh(){
+    int i = n;
+    while (i >= 1 && a[i] == 1){
+        a[i] = 0;
+        i--;
+    }
+    if (i == 0) final = 1;
+    else a[i] = 1;
+}
+
+bool check(int a[], int n, int k){
+    int cnt = 0;
+    for (int i = 1; i <= n; i++){
+        if (a[i] == 1) cnt++;
+    }
+    return cnt == k;
+}
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+    int k;
+    cin >> n >> k;
+    ktao();
+    while (final == 0){
+        if (check(a,n,k)){
+        for (int i = 1; i <= n; i++){
+            cout << a[i];
+        }
+        cout << '\n';
+        }
+        sinh();
+    }
+    final = 0;
+    }
+}
