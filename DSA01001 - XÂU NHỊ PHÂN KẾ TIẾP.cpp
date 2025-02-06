79f733262e8ce6d68a5179c86705d8c266124dee
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-06 10:01:20 +0700

    Create DSA01001 - XÂU NHỊ PHÂN KẾ TIẾP.cpp

---
+++ DSA01001 - XÂU NHỊ PHÂN KẾ TIẾP.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void sinh(int a[], int z){
+    int i = z-1;
+    while (i >= 1 && a[i] == 1){
+        a[i] = 0;
+        i--;
+    }
+    if (i != 0) a[i] = 1;
+
+    for (int i = 1; i < z; i++) cout << a[i];
+}
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        string s; cin >> s;
+        cin.ignore();
+        int n = s.size();
+        int a[1005]; a[0] = 0;
+        int z = 1;
+        for (char i : s){
+            a[z++] = (i - '0');
+        }
+        sinh(a,z);
+        cout << "\n";
+    }
+}
