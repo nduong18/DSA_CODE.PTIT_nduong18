efdc539479efb6e115c3eaaf0bc5357b98174728
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-06 10:07:02 +0700

    Create DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp

---
+++ DSA01007 - XÂU AB CÓ ĐỘ DÀI N.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int n, final = 0;
+char a[15];
+
+void ktao(){
+    for (int i = 1; i <= n; i++) a[i] = 'A';
+}
+
+void sinh(){
+    int i = n;
+    while (i >= 1 && a[i] == 'B'){
+        a[i] = 'A';
+        i--;
+    }
+    if (i == 0) final = 1;
+    else a[i] = 'B';
+}
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+    cin >> n;
+    ktao();
+    while (final == 0){
+        for (int i = 1; i <= n; i++){
+            cout << a[i];
+        }
+        cout << ' ';
+        sinh();
+    }
+    cout << "\n";
+    final = 0;
+    }
+}
