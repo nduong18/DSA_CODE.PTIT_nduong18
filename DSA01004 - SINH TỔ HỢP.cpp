7d45ab5c1e4cfb12696aa7f25b7c77cf19e7486b
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-06 21:47:12 +0700

    Create DSA01004 - SINH TỔ HỢP.cpp

---
+++ DSA01004 - SINH TỔ HỢP.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int n, k, a[100], final = 0;
+
+void ktao(){
+    for (int i = 1; i <= k; i++) a[i] = i;
+}
+
+void sinh(){
+    int i = k;
+    while (i >= 1 && a[i] == n-k+i){
+        i--;
+    }
+    if (i == 0) final = 1;
+    else{
+        a[i]++;
+        for (int j = i+1; j <= k; j++){
+            a[j] = a[j-1]+1;
+        }
+    }
+}
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+    cin >> n >> k;
+    ktao();
+    while (final == 0){
+        for (int i = 1; i <= k; i++) cout << a[i];
+        cout << ' ';
+        sinh();
+    }
+    cout << endl;
+    final = 0;
+    }
+}
