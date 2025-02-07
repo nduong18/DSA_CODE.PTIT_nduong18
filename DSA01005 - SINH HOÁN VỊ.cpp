7b45f4d088cc1ad2bcd11918223f4e53ef7b76b6
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-07 13:01:05 +0700

    Create DSA01005 - SINH HOÁN VỊ.cpp

---
+++ DSA01005 - SINH HOÁN VỊ.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//GITHUB: nduong18
+
+int n, a[100];
+
+void ktao(){
+    for (int i = 1; i <= n; i++) a[i] = i;
+}
+
+int main(){
+   int t; cin >> t;
+   while (t--){ 
+   cin >> n;
+   ktao();
+   vector <vector<int>> v;
+   do{
+        vector <int> x (a+1,a+n+1);
+        v.push_back(x);
+   } while (next_permutation(a+1,a+n+1));
+
+   for (int i = 0; i < v.size(); i++){
+        for (auto it : v[i]){
+            cout << it;
+        }
+        cout << ' ';
+   }
+   cout << endl;
+ }
+}
+
