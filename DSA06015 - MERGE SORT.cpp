aa0aa2a3ca6155af8e21868aacabea72552c970d
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-17 20:27:36 +0700

    Create DSA06015 - MERGE SORT.cpp

---
+++ DSA06015 - MERGE SORT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//GitHub: nduong18
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a,a+n);
+        for (int i : a) cout << i << ' ';
+        cout << endl;
+    }
+}
