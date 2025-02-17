5a8769cdeaf82c3f54df48fd5564f0bc6fa7b48b
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-17 11:22:49 +0700

    Create DSA06023 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.cpp

---
+++ DSA06023 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//GitHub: nduong18
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    for (int i = 0; i < n-1; i++){
+        for (int j =i+1; j < n; j++){
+            if (a[i] > a[j]) swap(a[i],a[j]);
+        }
+        cout << "Buoc " << i+1 << ": ";
+        for (int k = 0; k < n; k++) cout << a[k] << " ";
+        cout << endl;
+    }
+
+}
