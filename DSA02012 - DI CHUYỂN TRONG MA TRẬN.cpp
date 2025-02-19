bcaf7b76bc3924d9ea7c2178522f48c49e5b19bb
NDuong <139540304+nduong18@users.noreply.github.com>
2025-02-19 23:15:39 +0700

    Create DSA02012 - DI CHUYỂN TRONG MA TRẬN.cpp

---
+++ DSA02012 - DI CHUYỂN TRONG MA TRẬN.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//GitHub: nduong18
+
+int n, m, cnt = 0;
+
+void Try(int i, int j){
+    if (i == n-1 && j == m-1){
+        cnt++;
+        return;
+    }
+    if (i + 1 < n) Try(i+1,j);
+    if (j + 1 < m) Try(i,j+1);
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        cin >> n >> m;
+        int x;
+        for(int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> x;
+        Try(0,0);
+        cout << cnt << endl;
+        cnt = 0;
+    }
+}
