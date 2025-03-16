3ebf1a612721fbfdcf17012be48224d7fd64cef7
NDuong <139540304+nduong18@users.noreply.github.com>
2025-03-16 22:58:32 +0700

    Update DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp

--- DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
+++ DSA05004 - DÃY CON TĂNG DÀI NHẤT.cpp
@@ -8,12 +8,10 @@ int main() {
     int n; cin >> n;
     int a[n], f[n];
     for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) f[i] = 1;
-
     for (int i = 0; i < n; i++){
         f[i] = 1;
         for (int j = 0; j < i; j++){
-            if (a[j] < a[i]){
+            if (a[i] > a[j]){
                 f[i] = max(f[i],f[j]+1);         
             }
         }
