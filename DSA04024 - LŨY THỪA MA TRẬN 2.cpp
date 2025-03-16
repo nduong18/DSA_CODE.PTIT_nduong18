c549afd78a59282e2a26a420bdbbbabbd8688261
NDuong <139540304+nduong18@users.noreply.github.com>
2025-03-16 21:25:30 +0700

    Create DSA04024 - LŨY THỪA MA TRẬN 2.cpp

---
+++ DSA04024 - LŨY THỪA MA TRẬN 2.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+//Github: nduong18
+
+int n;
+
+struct Matrix
+{
+    ll f[12][12];
+};
+
+Matrix operator * (Matrix a, Matrix b){
+    Matrix c = {0};
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    c.f[i][j] = (c.f[i][j] + a.f[i][k] * b.f[k][j]) % mod;
+    return c;
+}
+
+Matrix powMod (Matrix a, ll n){
+    if (n == 1) return a;
+    Matrix x = powMod(a,n/2);
+    return (n % 2 == 0) ? x*x : a*x*x;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--){
+        int k; cin >> n >> k;
+        Matrix a;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> a.f[i][j];
+
+        Matrix res = powMod(a,k);
+
+        ll sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += res.f[i][n-1];
+            sum %= mod;
+        }
+        cout << sum << endl;
+    }
+}
