c828108f79196f2f436caa0685b012febd1927c3
NDuong <139540304+nduong18@users.noreply.github.com>
2025-03-17 09:39:00 +0700

    Create DSA05006 - TỔNG LỚN NHẤT CỦA DÃY CON TĂNG DẦN.cpp

---
+++ DSA05006 - TỔNG LỚN NHẤT CỦA DÃY CON TĂNG DẦN.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+//Github: nduong18
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        int a[n], f[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++){
+            f[i] = a[i];
+            for (int j = 0; j < i; j++){
+                if (a[i] > a[j]){
+                    f[i] = max(f[i],f[j]+a[i]);         
+                }
+            }
+        }
+        cout << *max_element(f,f+n) << endl;
+    }
+}
