8de9717d51b9f0dea3cf193c2ab7eb22ace9b3ac
NDuong <139540304+nduong18@users.noreply.github.com>
2025-05-09 17:33:56 +0700

    Create DSA07009 - BIẾN ĐỔI TIỀN TỐ - TRUNG TỐ.cpp

---
+++ DSA07009 - BIẾN ĐỔI TIỀN TỐ - TRUNG TỐ.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+
+    int t; cin >> t;
+    cin.ignore();
+    string s;
+    
+    while (t--){
+        cin >> s;
+        stack <string> st;
+
+        for (int i = s.size()-1; i >= 0; i--){
+            char c = s[i];
+            if (c == '+' || c == '-' || c == '*' || c == '/'){
+                string a = st.top(); st.pop();
+                string b = st.top(); st.pop();
+
+                string tmp = '(' + a + c + b + ')';
+                st.push(tmp);
+            }
+            else{
+                string num(1,c);
+                st.push(num);
+            }
+        }
+
+        cout << st.top() << '\n';
+    }
+}
