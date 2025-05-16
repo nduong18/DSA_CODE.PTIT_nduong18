9438ecd4d2fa4a30e005d34bf054085f46bed667
NDuong <139540304+nduong18@users.noreply.github.com>
2025-05-16 19:31:31 +0700

    Create DSA09025 - ĐƯỜNG ĐI THEO DFS VỚI ĐỒ THỊ CÓ HƯỚNG.cpp

---
+++ DSA09025 - ĐƯỜNG ĐI THEO DFS VỚI ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m,u,v;
+bool visited[1001];
+vector<int> ke[1001];
+bool check;
+string res;
+
+void DFS(int j, string s){
+    visited[j] = true;
+    for (int i : ke[j]){
+        if (!visited[i]){
+            if (i == v){
+                res = s + " " + to_string(i);
+                check = true;
+            }
+            DFS(i,s + " " + to_string(i));
+        }
+    }
+}
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int t; cin >> t;
+    while (t--){
+        memset(visited,false,sizeof(visited));
+        memset(ke,0,sizeof(ke));
+        check = false;
+        res = "";
+
+        cin >> n >> m >> u >> v;
+        for (int i = 0; i < m; i++){
+            int x,y; cin >> x >> y;
+            ke[x].push_back(y);
+        }
+        
+        DFS(u,to_string(u));
+
+        if (!check) cout << "-1\n";
+        else cout << res << '\n';
+    }
+
+}
