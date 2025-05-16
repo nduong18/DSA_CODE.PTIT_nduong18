554298592ff14170e35032003c3b9e130aa91dd2
NDuong <139540304+nduong18@users.noreply.github.com>
2025-05-16 15:56:41 +0700

    Create DSA09022 - DFS TRÊN ĐỒ THỊ CÓ HƯỚNG.cpp

---
+++ DSA09022 - DFS TRÊN ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool visited[1001];
+vector<int> ke[1001];
+
+void DFS(int u){
+    cout << u << ' ';
+    visited[u] = true;
+    for (int v : ke[u]){
+        if (!visited[v]){
+            DFS(v);
+        }
+    }
+}
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int t; cin >> t;
+    while (t--){
+        memset(ke,0,sizeof(ke));
+        memset(visited,false,sizeof(visited));
+
+        int v,e,u; cin >> v >> e >> u;
+        for (int i = 0; i < e; i++){
+            int x,y; cin >> x >> y;
+            ke[x].push_back(y);
+        }
+        DFS(u);
+        cout << "\n";
+
+    }
+
+}
+
