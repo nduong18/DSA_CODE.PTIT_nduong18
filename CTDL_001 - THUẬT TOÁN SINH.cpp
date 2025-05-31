42ec94b02a2b9a2d68316c2e5ac6b623291b8ae4
NDuong <139540304+nduong18@users.noreply.github.com>
2025-05-31 15:29:25 +0700

    Update CTDL_001 - THUẬT TOÁN SINH.cpp

--- CTDL_001 - THUẬT TOÁN SINH.cpp
+++ CTDL_001 - THUẬT TOÁN SINH.cpp
@@ -1,42 +1,35 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
-//GITHUB: nduong18
+
+int n;
+vector <string> res;
 
 bool check(string s){
     string tmp = s;
-    reverse(s.begin(),s.end());
+    reverse(s.begin(), s.end());
     return tmp == s;
 }
 
-int n, a[100], final = 0;
-
-void ktao(){
-    for (int i = 1; i <= n; i++) a[i] = 0;
-}
-
-void sinh(){
-    int i = n;
-    while (i >= 1 && a[i] == 1){
-        a[i] = 0;
-        i--;
+void backtrack(string s){
+    if (s.length() == n){
+        if (check(s)) res.push_back(s);
+        return;
     }
-    if (i == 0) final = 1;
-    else a[i] = 1;
+    backtrack(s + '0');
+    backtrack(s + '1');
 }
 
-int main(){
+signed main() {
+    cin.tie(0) -> sync_with_stdio(0);
+
     cin >> n;
-    ktao();
-    while (final == 0){
-        string tmp = "";
-        for (int i = 1; i <= n; i++) tmp += (a[i] + '0');
-        
-        if (check(tmp)){
-            for (int i = 1; i <= n; i++) cout << a[i] << ' ';
-            cout << "\n";
-            sinh();
+    backtrack("");
+    for (auto &i : res){
+        for (char c : i){
+            cout << c << ' ';
         }
-        sinh();
+        cout << '\n';
     }
+
+
 }
