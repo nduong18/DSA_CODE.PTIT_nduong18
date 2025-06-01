ab252574e0f8ee88aba04ec31487f90f22ce9c72
NDuong <139540304+nduong18@users.noreply.github.com>
2025-06-01 22:13:51 +0700

    Create DSA08018 - SỐ LỘC PHÁT 2.cpp

---
+++ DSA08018 - SỐ LỘC PHÁT 2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<string> v;
+    queue<string> q;
+    q.push("6");
+    q.push("8");
+    while (!q.empty()){
+        string x = q.front();
+        q.pop();
+        if (x.size() > n) break;
+        v.push_back(x);
+        q.push(x + "6");
+        q.push(x + "8");
+    }
+
+    cout << v.size() << '\n';
+    for (auto i : v) cout << i << ' ';
+    cout << '\n';
+}
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--){
+        solve();
+    }
+
+
+}
