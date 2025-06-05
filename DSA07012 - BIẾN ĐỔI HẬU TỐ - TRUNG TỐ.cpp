55e5704af74f3cd016ca461b70b53105aac3bf26
NDuong <139540304+nduong18@users.noreply.github.com>
2025-06-05 20:33:55 +0700

    Create DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp

---
+++ DSA07012 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    
+    int t; cin >> t; 
+    while (t--){
+        string s; cin >> s;
+        stack <string> st;
+
+        for (char c : s){
+            if (c == '+' || c == '-' || c == '*' || c == '/'){
+                string a = st.top(); st.pop();
+                string b = st.top(); st.pop();
+                string tmp = '(' + b + c + a + ')';
+                st.push(tmp);             
+            }
+            else {
+                string num(1,c);
+                st.push(num);
+            }
+        }
+        cout << st.top() << '\n';
+    }
+
+}
